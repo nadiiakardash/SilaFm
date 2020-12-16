87d90c7963342eb98b55d61be18ead3f7470bcfb
zmej-gorunuch <z.g-web@ukr.net>
2020-12-16 14:37:16 +0200

    sliders on pagespeakers.html

--- js/page-speaker.js
+++ js/page-speaker.js
@@ -1,4 +1,20 @@
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
 
 document.addEventListener('play', function(e){
   var audios = document.getElementsByTagName('input');
@@ -1051,3 +1067,15 @@ document.addEventListener('play', function(e){
       }
   }
 }, true);
+
+// $(document).ready(function(){
+//   $('.example-wrapper').slick({
+
+//   });
+// })
+$(document).ready(function(){    
+  $(".example-wrapper").easySlider({
+      auto: true, 
+      continuous: true
+  });
+});
